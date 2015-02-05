384bb29175244d86d077e5c72eacf5e5f933c90e
fangshuai <mafangshuai@126.com>
2015-02-06 00:18:46 +0800

    style.com collection crawler

---
+++ src/main/java/com/ng/daily/server/crawler/Start.java
@@ -0,0 +1,24 @@
+package com.ng.daily.server.crawler;
+
+import com.ng.daily.server.crawler.style.Style;
+
+import java.io.IOException;
+
+/**
+ * Created by fangs on 15/2/6.
+ */
+public class Start {
+
+
+    public static void main(String[] args) throws IOException {
+
+        Style style = new Style();
+        String saveDir = "/tmp/style.com";
+        String collectionUrl = "http://www.style.com/slideshows/fashion-shows/spring-2015-couture/chanel/collection";
+
+        style.download(saveDir, collectionUrl);
+
+    }
+
+
+}
